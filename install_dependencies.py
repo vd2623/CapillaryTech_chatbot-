b8bed8792c8bebe2489b0adcb4bbb5ad783cdfb5
vd2623 <132830931+vd2623@users.noreply.github.com>
2025-04-10 11:12:09 +0530

    Create install_dependencies.py

---
+++ install_dependencies.py
@@ -0,0 +1,4 @@
+!pip install -q beautifulsoup4 requests langchain openai chromadb sentence-transformers python-dotenv
+!pip install -q langchain-huggingface
+!pip install -U langchain langchain-community langchain-huggingface chromadb sentence-transformers
+!pip install faiss-cpu
